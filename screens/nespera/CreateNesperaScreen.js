9128f1e698b27e9c79a5adb682faeec21e49e7a8
José Maria Pereira <zemaria@gmail.com>
2020-01-11 01:14:01 +0000

    dropped db

--- screens/nespera/CreateNesperaScreen.js
+++ screens/nespera/CreateNesperaScreen.js
@@ -1,49 +1,49 @@
-import React,{useState,useContext} from "react";
-import { View, Text,StyleSheet } from "react-native";
-import {TextInput,Button} from "react-native-paper";
+import React, { useState, useContext } from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { TextInput, Button } from "react-native-paper";
 import { store } from '../../store/store';
 
 const CreateNesperaScreen = (props) => {
- const [title, setTitle] = useState("");
- const [opA, setOpA] = useState("");
- const [opB, setOpB] = useState("");
-   
+  const [title, setTitle] = useState("");
+  const [opA, setOpA] = useState("");
+  const [opB, setOpB] = useState("");
 
- const globalState = useContext(store);
-const user = globalState.state.user;
-console.log(user);
- 
-const handleCreate = async () =>{
-  const response = await fetch("https://ironhack-wouldyourather.herokuapp.com/api/create", {
-    method:"POST",
-    headers:{"Content-Type":"application/json"},
-    body:JSON.stringify({ optionA:opA, optionB:opB, category:"dollar", title:title,authorID:user })
-  });
-const resData = await response.json();
-console.log(resData);
- 
-  
-props.navigation.pop();
-}
 
- 
+  const globalState = useContext(store);
+  const user = globalState.state.user;
+  console.log(user);
+
+  const handleCreate = async () => {
+    const response = await fetch("https://ironhack-wouldyourather.herokuapp.com/api/create", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ optionA: opA, optionB: opB, category: "dollar", title: title, authorID: user })
+    });
+    const resData = await response.json();
+    console.log(resData);
+
+
+    props.navigation.pop();
+  }
+
+
 
   return (
     <View style={styles.container}>
-      <TextInput placeholder="Título do Dilema" onChangeText={(text)=>setTitle(text)} />
+      <TextInput placeholder="Título do Dilema" onChangeText={(text) => setTitle(text)} />
       <Text>Preferias</Text>
-      <TextInput placeholder="Ser o Nuno Markl." onChangeText={(text)=>setOpA(text)} />
+      <TextInput placeholder="Ser o Nuno Markl." onChangeText={(text) => setOpA(text)} />
       <Text>ou</Text>
-      <TextInput key="asd" placeholder="Ser Anão" onChangeText={(text)=>setOpB(text)} />
-      <Button onPress={()=>handleCreate()}>Criar</Button>
+      <TextInput key="asd" placeholder="Ser Anão" onChangeText={(text) => setOpB(text)} />
+      <Button onPress={() => handleCreate()}>Criar</Button>
     </View>
   );
 };
 const styles = StyleSheet.create({
   container: {
-    flex:1,
-    justifyContent:"center",
-    paddingHorizontal:30
+    flex: 1,
+    justifyContent: "center",
+    paddingHorizontal: 30
   }
 })
 
