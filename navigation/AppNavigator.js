1c8b1cb0e475d02f4ad1565924ea336a25b7f127
José Maria Pereira <zemaria@gmail.com>
2020-01-13 10:12:39 +0000

    env configs

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -7,7 +7,7 @@ export default createAppContainer(
     // You could add another route here for authentication.
     // Read more at https://reactnavigation.org/docs/en/auth-flow.html
 
-    Main: MainTabNavigator,
     Auth: AuthNavigator,
+    Main: MainTabNavigator,
   })
 );
